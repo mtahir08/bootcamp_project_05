fa2dfddaf3bcb44a62f3c1e6c48776fa055df807
Muhammad osama khan <khanosama1996@gmail.com>
2020-03-07 16:22:56 +0500

    loginAndSignup

--- src/input.js
+++ src/input.js
@@ -1,71 +1,53 @@
 import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import './index.css';
-import { styles } from './style';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+import './index.css'
+import { styles } from './style'
 import TodoActions from './store/Actions/index';
 
 function mapStateToProps(state) {
-	return {
-		editingItem: state.editingItem
-	};
+    return {
+        editingItem: state.editingItem
+    }
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		// Add: (obj) => dispatch(TodoActions.Add(obj))
-		Add: (obj) => dispatch({ type: 'ADD', payload: obj }),
-		// update: (obj) => dispatch(TodoActions.update(obj))
-		Update: (obj) => dispatch({ type: 'UPDATE', payload: obj })
-	};
+    return {
+        Add: (obj) => dispatch(TodoActions.Add(obj)),
+        Update: (obj) => dispatch(TodoActions.Update(obj))
+    }
 }
 
 const Input = (props) => {
-	// console.log(props);
-	const [todo, setTodo] = useState('');
-	useEffect(() => {
-		console.log(props.editingItem);
-		if (props.editingItem) {
-			setTodo(props.editingItem.name);
-		}
-	}, [props.editingItem]);
+    // console.log(props);
+    const [todo, setTodo] = useState("")
+    useEffect(() => {
+        console.log(props.editingItem);
+        if (props.editingItem) {
+            setTodo(props.editingItem.text)
+        }
+    }, [props.editingItem])
 
-	return (
-		<div>
-			<input
-				type="text"
-				onChange={(event) => {
-					setTodo(event.target.value);
-				}}
-				value={todo}
-				// className="input"
-				style={styles.input}
-			/>
-			<button
-				onClick={() => {
-					if (todo.length) {
-						if (props.editingItem) {
-							props.Update({
-								...props.editingItem,
-								name: todo
-							});
-						} else {
-							props.Add({
-								id: parseInt(Math.random() * 10000),
-								name: todo
-							});
-						}
-					}
-				}}
-			>
-				{' '}
-				{props.editingItem ? 'Update' : 'Add'}{' '}
-			</button>
-			<Link to="/">
-				<button className="button">Go Back</button>
-			</Link>
-		</div>
-	);
-};
+    return <div>
+        <input
+            type="text"
+            onChange={(event) => { setTodo(event.target.value) }}
+            value={todo}
+            // className="input"
+            style={styles.input}
+        />
+        <button onClick={() => {
+            if (todo.length) {
+                if (props.editingItem) {
+                    props.Update({
+                        ...props.editingItem,
+                        text: todo
+                    })
+                } else {
+                    props.Add({ text: todo })
+                }
+            }
+        }}> {props.editingItem ? "Update" : "Add"} </button>
+    </div >
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Input);
+export default connect(mapStateToProps, mapDispatchToProps)(Input)
--- src/list.js
+++ src/list.js
@@ -2,69 +2,52 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
 import TodoActions from './store/Actions/index';
-import { Link } from 'react-router-dom';
+
 
 function mapStateToProps(state) {
-	return {
-		todos: state.todos
-	};
+    return {
+        todos: state.todos
+    }
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		// delete: (id) => {
-		//     return dispatch(TodoActions.Delete(id))
-		// },
-		delete: (id) => {
-			return dispatch({ type: 'DELETE', payload: id });
-		},
-		// edit: (obj) => dispatch(TodoActions.Edit(obj))
-		edit: (obj) => dispatch({ type: 'EDIT', payload: obj })
-	};
+    return {
+        get: () => dispatch(TodoActions.Get()),
+        delete: (id) => dispatch(TodoActions.Delete(id)),
+        edit: (obj) => dispatch(TodoActions.Edit(obj)),
+    }
 }
 
-const List = (props) => {
-	let rows = props.todos.map((item, i) => (
-		<tr key={i}>
-			<td>{item.id}</td>
-			<td>{item.name}</td>
-			<td>
-				<button
-					onClick={() => {
-						props.edit(item);
-					}}
-				>
-					Edit
-				</button>
-				<button
-					onClick={() => {
-						props.delete(item.id);
-					}}
-				>
-					Delete
-				</button>
-			</td>
-		</tr>
-	));
 
-	return (
-		<div>
-			<table>
-				<thead>
-					<tr>
-						<th>S.no</th>
-						<th>Name</th>
-						{/* <th>Class</th> */}
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>{rows}</tbody>
-			</table>
-			<Link to="/input">
-				<button className="button">Create Todo</button>
-			</Link>
-		</div>
-	);
-};
+const List = (props) => {
+
+    useEffect(() => {
+        props.get();
+    }, [])
+
+    let rows = props.todos.map((item, i) => (
+        <tr key={i}>
+            <td>{(i + 1)}</td>
+            <td>{item.text}</td>
+            <td>
+                <button onClick={() => { props.edit(item) }}>Edit</button>
+                <button onClick={() => { props.delete(item._id) }}>Delete</button>
+            </td>
+        </tr>
+    ));
+
+    return <table>
+        <thead>
+            <tr>
+                <th>S.no</th>
+                <th>Name</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            {rows}
+        </tbody>
+    </table>
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(List);
