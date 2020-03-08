294db3fe4be49e47ef549eb82e8423f3792b2c69
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2020-03-08 04:04:24 -0700

    Showing player details in dashboard

--- src/components/Dashboard/UserTable.js
+++ src/components/Dashboard/UserTable.js
@@ -5,15 +5,9 @@ function UserTable() {
     
             <thead>
             <tr>
-                <th>#</th>
-                <th>Name</th>
                 <th>Email</th>
-                <th>Phone</th>
-                <th>Association Office Name</th>
+                <th>Gender</th>
                 <th>Year</th>
-                <th>Street</th>
-                <th>City</th>
-                <th>Company</th>
                 <th>Action</th>
             </tr>
             </thead>
--- src/components/Dashboard/UserTableDetail.js
+++ src/components/Dashboard/UserTableDetail.js
@@ -6,15 +6,9 @@ function UserDetail(props) {
   const userData = props.userData.map(user => {
     return (
       <tr key={user.id}>
-        <td>{user.id}</td>
-        <td>{user.name}</td>
         <td>{user.email}</td>
-        <td>{user.phone}</td>
-        <td>{user.company.bs}</td>
-        <td>2003</td>
-        <td>{user.address.street}</td>
-        <td>{user.address.city}</td>
-        <td>{user.company.name}</td>
+        <td>{user.gender}</td>
+        <td>{user.createdAt.split('-')[0]}</td>
         <td>
         <Link to="/dashboard">Go Back</Link>
         <br/>
