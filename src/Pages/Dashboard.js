7bb1020c93957ccf9a2fea5e75788df3d7ba8165
Muhammad Tahir <mtahir.ps@gmail.com>
2020-03-14 19:04:18 +0500

    fix breaking issue in dashboard

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -10,54 +10,54 @@ import { useParams, useHistory } from 'react-router-dom';
 import UserTable from '../components/Dashboard/UserTable';
 import UserTableDetail from '../components/Dashboard/UserTableDetail';
 
-import Side from '../Sidenav';
-
 const Dash = (props) => {
 	const [data, setData] = useState([]);
 	useEffect(() => {
-		
+
 		didMount();
 	}, [props.users]);
 
 	const didMount = () => {
 		const tokenObj = localStorage.getItem('token');
-	
+
 		let url = process.env.REACT_APP_DASHBOARDAPI;
 
 		fetch(url, {
 			method: 'GET',
 			headers: {
 				'Content-Type': 'application/json',
-				"Authorization": `Bearer ${tokenObj}`  
+				"Authorization": `Bearer ${tokenObj}`
 			}
 		})
 			.then((data) => {
-				data.json()
-					.then(res2 => {
-					
-						setData({ data: res2.data.user })
-
-					})
+				if (data.status === 200) {
+					return data.json()
+				}
+				throw data;
+			})
+			.then(res2 => {
+				console.log({ res2 });
+				setData({ data: res2.data.user })
 			})
 			.catch((error) => {
-				
+				console.log(error)
 			});
 	};
 
 	return (
-		
-			
-				<Table className="table" >
-					<UserTable />
-					<UserTableDetail userData={data.data} />
-				</Table>
-			
-		
+
+
+		<Table className="table" >
+			<UserTable />
+			<UserTableDetail userData={data.data} />
+		</Table>
+
+
 	);
 };
 
 function mapStateToProps(state) {
-	
+
 	return {
 		user: state.users,
 		token: state.token
@@ -82,7 +82,7 @@ function UserDetail() {
 	useEffect(() => {
 		setIsLoading(true);
 		let url = process.env.REACT_APP_DASHBOARDAPI;
-		
+
 		fetch(url + userId)
 			.then((response) => response.json())
 			.then((response) => {
@@ -92,15 +92,15 @@ function UserDetail() {
 	}, []);
 
 	return (<Table className="table">
-				<UserTable />
+		<UserTable />
 
-				{isLoading ? (
-					<h1>Loading...</h1>
-				) : (
-						<UserTableDetail userData={userDetail} />
-					)}
-			</Table>
-		
+		{isLoading ? (
+			<h1>Loading...</h1>
+		) : (
+				<UserTableDetail userData={userDetail} />
+			)}
+	</Table>
+
 	);
 }
 export { UserDetail };
