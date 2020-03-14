a1c7eb4068fdd1bed34a29050cc3c07a752ee184
Muhammad Tahir <mtahir.ps@gmail.com>
2020-03-14 19:47:23 +0500

    remove unusued

--- src/Pages/ReceiptDetail.js
+++ src/Pages/ReceiptDetail.js
@@ -1,157 +1,160 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Table } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+
 import receiptDetailAction from "../store/Actions/ReceiptDetailAction"
-import {connect} from 'react-redux'
 import Img from "./images/images.png"
-import ".././index.css"
+import "../../index.css"
 
 function mapDispatchToProps(dispatch) {
-	return {
-		SET_DATA: (obj) => {
-			console.log(obj);
-			dispatch(receiptDetailAction.setData(obj));
-		}
-	};
+    return {
+        SET_DATA: (obj) => {
+            dispatch(receiptDetailAction.setData(obj));
+        }
+    };
 }
-function mapStateToProps(state){
-    console.log(state.receiptDetailReducer.Data)
-    return{
-        Data  :state.receiptDetailReducer.Data
+
+function mapStateToProps(state) {
+    return {
+        Data: state.receiptDetailReducer.Data
     }
 }
 
 class ReceiptDetails extends Component {
     state = {
-        isLoading : false,
-        data : [],
-        UserId : "",
-        status : ""
+        isLoading: false,
+        data: [],
+        UserId: "",
+        status: ""
 
     }
-    async componentDidMount(){
-        const user =await this.props.match.params.userId
-       await this.setState({UserId : user})
-      await this.props.SET_DATA(this.state.UserId)
-          await this.setState({data :this.props.Data,isLoading : true})
-       }
-        ApproveData = ()=>{
-            fetch("https://uitedemo.herokuapp.com/api/receipt/status/approved", {
-                method: "PUT",
-                body:JSON.stringify( {
-                    "_id" : `${this.state.UserId}`
-                }),
-                headers: {
-                "Content-type": "application/json",
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZTZjYzMxMmVjYWFhODAwMDQ2NzRjN2UiLCJyb2xlIjoiQSIsImV4cCI6MTU4NDE4OTc1MSwiaWF0IjoxNTg0MTg2MTUxfQ.WKnY76tD-b4rtj_aQ7wUWAqT1t_f_i2RYbYql0j0xyk"}
-                })
-                .then(response => response.json())
-                .then(res2 =>{
-                    if(res2.data.status === "approved"){
-                    }
 
-                 })
+    async componentDidMount() {
+        const user = await this.props.match.params.userId
+        await this.setState({ UserId: user })
+        await this.props.SET_DATA(this.state.UserId)
+        await this.setState({ data: this.props.Data, isLoading: true })
     }
-        disApproveData = ()=>{
-            fetch("https://uitedemo.herokuapp.com/api/receipt/status/declined", {
-                method: "PUT",
-                body:JSON.stringify( {
-                    "_id" : `${this.state.UserId}`
-                }),
-                headers: {
+
+    ApproveData = () => {
+        fetch("https://uitedemo.herokuapp.com/api/receipt/status/approved", {
+            method: "PUT",
+            body: JSON.stringify({
+                "_id": `${this.state.UserId}`
+            }),
+            headers: {
                 "Content-type": "application/json",
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZTZjZDkxMTI4YmI4ZTAwMDRmMWQxMGYiLCJyb2xlIjoiQSIsImV4cCI6MTU4NDE5OTAyMCwiaWF0IjoxNTg0MTk1NDIwfQ.Gk6QPDkzNSRlj-tOxJ2F7YF-nqcVLhZjnf3oze7aAzU"}
-                })
-                .then(response => response.json())
-                .then(res2 =>{
-                })
-    }
-    render(){
-        console.log(this.props.Data)
-        return (
-        <div className="container">
-             
-{this.state.isLoading ? this.props.Data.map((user,i) => {
-    return (  
-        <Table key={i} striped bordered hover>
-               <tbody> <tr>
-                        <th>#</th>
-                        <td style={{"width":"550px"}}>{i}</td>
-                    </tr>
-                    <tr>
-                        <th>ID</th><td>{user._id}</td>
-                    </tr>
-                    <tr>
-                        <th>Sent At</th>
-                        <td>{user.sentAt}</td>
-                    </tr>
-                    <tr>
-                        <th>Name</th>
-                        <td>{user.sentBy.name}</td>
-                    </tr>
-                    <tr>
-                        <th>Email</th>
-                        <td>{user.sentBy.email}</td>
-                    </tr>
-                    <tr>    
-                        <th>Created At</th>
-                        <td>{user.createdAt}</td>
-                        </tr>
-                    <tr>    
-                        <th>updated At</th>
-                        <td>{user.updatedAt}</td>
-                        </tr>
-                    <tr>
-                        <th>Year</th>
-                        <td>{user.year}</td>
-                        </tr>
-                    <tr>
-                        <th>Month</th>   
-                        <td>{user.month}</td>
-                        </tr>
-                    <tr>
-                        <th>Status</th>   
-                        <td>{user.status}</td>
-                        </tr>
-                    <tr>
-                        <th>Image</th> 
-                        <td>
-                            {
-                            this.state.isLoading ?
-                                user.url !== "" ?
-                             <img src={Img} alt="Image" /> : <img src={user.url} alt="Image" />
-                             : ""
-                            } 
-                             
-                            </td>
-                        </tr>
-                    <tr>
-                        <th>Status</th>
-                    <td>
-                        <button onClick={()=>this.ApproveData()}>Approval</button>&nbsp;&nbsp;
-                        <button onClick={()=>this.disApproveData()}>disApproval</button>&nbsp;&nbsp;
-                        <button>History</button> 
-                     </td>
-                        </tr>
-                    <tr>
-                        <th>Back</th><td><Link to="/receipt">Go Back</Link></td>
-                        </tr>
-                   </tbody>
-                   
-                  
-                </Table> 
-                
-             
-             
-        )
-    })
-   
-: "Loading"
-           
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZTZjYzMxMmVjYWFhODAwMDQ2NzRjN2UiLCJyb2xlIjoiQSIsImV4cCI6MTU4NDE4OTc1MSwiaWF0IjoxNTg0MTg2MTUxfQ.WKnY76tD-b4rtj_aQ7wUWAqT1t_f_i2RYbYql0j0xyk"
+            }
+        })
+            .then(response => response.json())
+            .then(res2 => {
+                if (res2.data.status === "approved") {
                 }
-        </div>
-    )
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
-    
+
+    disApproveData = () => {
+        fetch("https://uitedemo.herokuapp.com/api/receipt/status/declined", {
+            method: "PUT",
+            body: JSON.stringify({
+                "_id": `${this.state.UserId}`
+            }),
+            headers: {
+                "Content-type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZTZjZDkxMTI4YmI4ZTAwMDRmMWQxMGYiLCJyb2xlIjoiQSIsImV4cCI6MTU4NDE5OTAyMCwiaWF0IjoxNTg0MTk1NDIwfQ.Gk6QPDkzNSRlj-tOxJ2F7YF-nqcVLhZjnf3oze7aAzU"
+            }
+        })
+            .then(response => response.json())
+            .then(res2 => {
+                console.log(res2);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    render() {
+        return (
+            <div className="container">
+
+                {this.state.isLoading ? this.props.Data.map((user, i) => {
+                    return (
+                        <Table key={i} striped bordered hover>
+                            <tbody> <tr>
+                                <th>#</th>
+                                <td style={{ "width": "550px" }}>{i}</td>
+                            </tr>
+                                <tr>
+                                    <th>ID</th><td>{user._id}</td>
+                                </tr>
+                                <tr>
+                                    <th>Sent At</th>
+                                    <td>{user.sentAt}</td>
+                                </tr>
+                                <tr>
+                                    <th>Name</th>
+                                    <td>{user.sentBy.name}</td>
+                                </tr>
+                                <tr>
+                                    <th>Email</th>
+                                    <td>{user.sentBy.email}</td>
+                                </tr>
+                                <tr>
+                                    <th>Created At</th>
+                                    <td>{user.createdAt}</td>
+                                </tr>
+                                <tr>
+                                    <th>updated At</th>
+                                    <td>{user.updatedAt}</td>
+                                </tr>
+                                <tr>
+                                    <th>Year</th>
+                                    <td>{user.year}</td>
+                                </tr>
+                                <tr>
+                                    <th>Month</th>
+                                    <td>{user.month}</td>
+                                </tr>
+                                <tr>
+                                    <th>Status</th>
+                                    <td>{user.status}</td>
+                                </tr>
+                                <tr>
+                                    <th>Image</th>
+                                    <td>
+                                        {
+                                            this.state.isLoading ?
+                                                user.url !== "" ?
+                                                    <img src={Img} alt="Image" /> : <img src={user.url} alt="Image" />
+                                                : ""
+                                        }
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>Status</th>
+                                    <td>
+                                        <button onClick={() => this.ApproveData()}>Approval</button>&nbsp;&nbsp;
+                        <button onClick={() => this.disApproveData()}>disApproval</button>&nbsp;&nbsp;
+                        <button>History</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>Back</th><td><Link to="/receipt">Go Back</Link></td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    )
+                })
+                    : "Loading"
+                }
+            </div>
+        )
+    }
+
 }
 export default connect(mapStateToProps, mapDispatchToProps)(ReceiptDetails) 
